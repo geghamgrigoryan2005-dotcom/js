6c251ccf3e80938dece40fcece4973c9d8c22283
geghamgrigoryan2005 <gegham.grigoryan2005@gmail.com>
2026-06-18 17:27:23 +0400

    first

---
+++ js-11.js
@@ -0,0 +1,31 @@
+// const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const ardyunq = []
+// arr1.forEach((elemets) => {
+//     console.log(elemets);
+//     arr2.forEach((element) => {
+//         console.log(`${element} * ${elemets} = ${elemets * element}`);
+      
+//     })
+// });
+
+    
+// for(let i  of arr1){
+//     console.log(i);
+    
+// }
+
+// console.log(Math.floor(Math.random() * (20 - 10 + 1)) + 10);
+
+const numbers = [5, 7, 44, 21, 77, 888, -20, -99, 7.8]
+const sorted = []
+
+numbers.forEach ((el) => {
+    if (i === -1
+        
+    ){
+        
+        console.log(sorted.push(el));
+    }
+    
+})
