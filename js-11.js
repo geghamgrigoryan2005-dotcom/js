4f191774686b3573b129d5b22160bc4867141ec2
geghamgrigoryan2005 <gegham.grigoryan2005@gmail.com>
2026-06-20 17:33:07 +0400

    1 ban

--- js-11.js
+++ js-11.js
@@ -5,27 +5,41 @@
 //     console.log(elemets);
 //     arr2.forEach((element) => {
 //         console.log(`${element} * ${elemets} = ${elemets * element}`);
-      
+
 //     })
 // });
 
-    
 // for(let i  of arr1){
 //     console.log(i);
-    
+
 // }
 
 // console.log(Math.floor(Math.random() * (20 - 10 + 1)) + 10);
 
-const numbers = [5, 7, 44, 21, 77, 888, -20, -99, 7.8]
-const sorted = []
 
-numbers.forEach ((el) => {
-    if (i === -1
-        
-    ){
-        
-        console.log(sorted.push(el));
+// let numbersSorted = []
+
+// numbers.forEach ((num) => {
+//     if (num === 0){
+//         numbersSorted.unshift(num)
+//     }else{
+//         numbersSorted.push(num)
+//     }
+
+// })
+
+// console.log(numbersSorted);
+const numbers = [5, 0, 7, 0,  44, 0, 21, 0,  77, 0, 888, -20, -99, 7.8];
+let nums = {
+    even:[],
+    odd:[]
+} 
+numbers.forEach((num) => {
+    if(num % 2 == 0){
+        nums.even.unshift(num)
+    }else{
+        nums.odd.unshift(num)
     }
-    
 })
+
+console.log(nums);
